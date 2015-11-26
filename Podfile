bf58d2eb1de9933eb5393042c71b47a13aa55124
Klein Mioke <kleinmac@XiKangdeMacBook-Pro.local>
2015-11-26 15:37:52 +0800

    Add FMDB SDK

--- Podfile
+++ Podfile
@@ -5,3 +5,9 @@ pod 'Alamofire', '~> 3.0'
 
 pod 'ReactiveCocoa', '~> 4.0.4-alpha-1'
 pod "AFNetworking", "~> 2.0"
+
+pod 'FMDB'
+# pod 'FMDB/FTS'   # FMDB with FTS
+# pod 'FMDB/standalone'   # FMDB with latest SQLite amalgamation source
+# pod 'FMDB/standalone/FTS'   # FMDB with latest SQLite amalgamation source and FTS
+# pod 'FMDB/SQLCipher'   # FMDB with SQLCipher
--- Pods/Target Support Files/Pods/Pods-frameworks.sh
+++ Pods/Target Support Files/Pods/Pods-frameworks.sh
@@ -86,12 +86,14 @@ strip_invalid_archs() {
 if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "Pods/AFNetworking.framework"
   install_framework "Pods/Alamofire.framework"
+  install_framework "Pods/FMDB.framework"
   install_framework "Pods/ReactiveCocoa.framework"
   install_framework "Pods/Result.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "Pods/AFNetworking.framework"
   install_framework "Pods/Alamofire.framework"
+  install_framework "Pods/FMDB.framework"
   install_framework "Pods/ReactiveCocoa.framework"
   install_framework "Pods/Result.framework"
 fi

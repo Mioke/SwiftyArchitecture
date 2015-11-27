dcba0cef9765aebdae44f238f29e8be761dcf300
Klein Mioke <kleinmac@XiKangdeMacBook-Pro.local>
2015-11-27 14:35:25 +0800

    change subclass of UIViewController to category

---
+++ swiftArchitecture/Base/KMViewController/UIViewController+TaskManager.swift
@@ -0,0 +1,9 @@
+//
+//  UIViewController+TaskManager.swift
+//  swiftArchitecture
+//
+//  Created by Klein Mioke on 15/11/27.
+//  Copyright © 2015年 KleinMioke. All rights reserved.
+//
+
+import Foundation

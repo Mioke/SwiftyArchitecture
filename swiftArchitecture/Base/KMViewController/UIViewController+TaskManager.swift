7449d752343019c2a0bfa51c673b7fe955f41910
Klein Mioke <565408238@qq.com>
2015-11-27 14:39:46 +0800

    Change subclass from UIViewController to category instead

--- swiftArchitecture/Base/KMViewController/UIViewController+TaskManager.swift
+++ swiftArchitecture/Base/KMViewController/UIViewController+TaskManager.swift
@@ -7,3 +7,37 @@
 //
 
 import Foundation
+
+extension UIViewController: TaskExecutor {
+    
+    typealias receivDataType = AnyObject
+    
+    func doTask(task: () -> receivDataType, identifier: String) {
+        
+        dispatch_async(dispatch_get_global_queue(0, 0)) { () -> Void in
+            
+            let result = task()
+            dispatch_async(dispatch_get_main_queue(), { () -> Void in
+                self.finishTaskWithReuslt(result, identifier: identifier)
+            })
+        }
+    }
+    
+    func doTask(task: () -> receivDataType, callBack: (receivDataType) -> Void) {
+        
+        dispatch_async(dispatch_get_global_queue(0, 0)) { () -> Void in
+            
+            let result = task()
+            dispatch_async(dispatch_get_main_queue(), { () -> Void in
+                callBack(result)
+            })
+        }
+    }
+    
+    func finishTaskWithReuslt(result: receivDataType, identifier: String) {
+        
+        if let _ = (result as? ResultType<receivDataType>)?.error() {
+            // do something
+        }
+    }
+}
--- swiftArchitecture/Demo/ViewController/ViewController.swift
+++ swiftArchitecture/Demo/ViewController/ViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: KMViewController {
+class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()

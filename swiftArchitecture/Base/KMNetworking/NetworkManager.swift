502b7f683605785f974f37e94191d4e62b9c03cd
Klein <565408238@qq.com>
2016-03-10 09:55:58 +0800

    Recover method of request

--- swiftArchitecture/Base/KMNetworking/NetworkManager.swift
+++ swiftArchitecture/Base/KMNetworking/NetworkManager.swift
@@ -11,37 +11,37 @@ import AFNetworking
 
 class NetworkManager: NSObject {
 
-//    class func sendRequestOfURL(url: NSURL, method: String, param: [String : AnyObject]?, timeout: NSTimeInterval?) -> AFHTTPRequestOperation {
-//        
-//        switch method {
-//        case "POST":
-//            return POST(url, param: param, timeout: timeout)
-//        default:
-//            return AFHTTPRequestOperation()
-//        }
-//    }
-//    
-//    private class func POST(url: NSURL, param: [String : AnyObject]?, timeout: NSTimeInterval?) -> AFHTTPRequestOperation {
-//        
-//        let request = NSMutableURLRequest(URL: url)
-//        request.HTTPMethod = "POST"
-//        request.timeoutInterval = timeout ?? 10
-//        
-//        if param != nil {
-//            var bodyString = ""
-//            for key in param!.keys {
-//                bodyString += "&\(key)=\(param![key])"
-//            }
-//            bodyString = bodyString.substringFromIndex(bodyString.startIndex.successor())
-//            request.HTTPBody = bodyString.dataUsingEncoding(NSUTF8StringEncoding)
-//        }
-//        
-//        let operation = AFHTTPRequestOperation(request: request)
-//        operation.start()
-//        operation.waitUntilFinished()
-//        
-//        return operation
-//    }
+    class func sendRequestOfURL(url: NSURL, method: String, param: [String : AnyObject]?, timeout: NSTimeInterval?) -> AFHTTPRequestOperation {
+        
+        switch method {
+        case "POST":
+            return POST(url, param: param, timeout: timeout)
+        default:
+            return AFHTTPRequestOperation()
+        }
+    }
+    
+    private class func POST(url: NSURL, param: [String : AnyObject]?, timeout: NSTimeInterval?) -> AFHTTPRequestOperation {
+        
+        let request = NSMutableURLRequest(URL: url)
+        request.HTTPMethod = "POST"
+        request.timeoutInterval = timeout ?? 10
+        
+        if param != nil {
+            var bodyString = ""
+            for key in param!.keys {
+                bodyString += "&\(key)=\(param![key])"
+            }
+            bodyString = bodyString.substringFromIndex(bodyString.startIndex.successor())
+            request.HTTPBody = bodyString.dataUsingEncoding(NSUTF8StringEncoding)
+        }
+        
+        let operation = AFHTTPRequestOperation(request: request)
+        operation.start()
+        operation.waitUntilFinished()
+        
+        return operation
+    }
     
     class func dealError(error: ErrorResultType) -> Void {
         
